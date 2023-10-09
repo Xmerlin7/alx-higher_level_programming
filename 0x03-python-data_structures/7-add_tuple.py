41a3218011b1ac8fe1cb2e3dd796c54a375b82bc
Seif Allah <saifeldarageely@gmail.com>
2023-10-09 09:42:44 -0700

    add_tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuble_a) < 2:
-        if len(tuble_a) == 0:
-            tuble_a = (0, 0)
-        else:
-            tuble_a[1] = 0
-    if len(tuble_b) < 2:
-        if len(tuble_b) == 0:
-            tuble_b = (0, 0)
-        else:
-            tuble_b[1] = 0
-    return (tuple_a[0] + tuble_b[0], tuple_a[1] + tuble_b[1])
-
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
