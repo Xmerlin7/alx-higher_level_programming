d6ab98adcfed59734dc11fe000a5d73720c92e41
Seif Allah <saifeldarageely@gmail.com>
2023-10-09 08:25:05 -0700

    ptint-alpha

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    x = [i for i in my_string if x != 'c' and x != 'C']
+    x = [i for i in my_string if i !='c' or \
+            i != 'C']
     return ("".join(x))
