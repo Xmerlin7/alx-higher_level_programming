4e1d272fe1d9055352a14254aaa9b49303eafd0b
Seif Allah <saifeldarageely@gmail.com>
2023-10-24 13:37:04 -0700

    class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" square module."""
+
+
+class Square:
+    """Define a square."""
+    def __init__(self, size=0):
+        """constr
+
+        Args:
+            size: length of a side of the square.
+
+        Raises:
+            TypeError: if size not int
+            ValueError: if size < 0
+        """
+        self.__size = size
+        
+        @property
+        def size(self):
+            """property for the length of the square.
+            aises:
+                ypeError: if size not int
+                alueError: if size < 0
+            """
+            return self.__size
+
+        
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError('size must be >= 0')
+
+    def area(self):
+        """ define a public instance method.
+
+
+        Returns:
+            size of the square
+        """
+        return self.__size ** 2
+
