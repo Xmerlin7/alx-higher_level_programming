4f44c43f849fb5684d8acc34cfe9c277bbf9f175
Seif Allah <saifeldarageely@gmail.com>
2023-10-24 13:12:37 -0700

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" square module."""
+
+
+class Square:
+    """Define a square."""
+    def __init__(self, size):
+        """constr
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
