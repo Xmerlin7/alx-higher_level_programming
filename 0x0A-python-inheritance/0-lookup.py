30ec007602acffe4240908c4d05a46b2d32096b3
Seif Allah <saifeldarageely@gmail.com>
2024-05-28 19:44:57 +0300

    #

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,7 +5,7 @@ def lookup(obj):
     '''Looks up object attributes and methods.
     Args:
         obj (object): the object to list.
-
+        
     Returns:
         list: the list of attributes.
     '''
