819d68d128ba0cf52ffebb935cec9b0ff41cce72
Seif Allah <saifeldarageely@gmail.com>
2024-05-30 20:37:20 +0300

    #

---
+++ 0x0C-python-almost_a_circle/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/dev python3
+
+def addNumbers(a, b):
+    """
+    Add two Numbers
+    
+    This functions takes two numbers a, b and adds them
+    
+    usage:
+    >>> addNumbers(5, 2)
+    8
+    """
+    return (a + b)
