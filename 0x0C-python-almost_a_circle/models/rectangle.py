e8b6812d82e1559211b3cca2ef0af9cd08264533
Seif Allah <saifeldarageely@gmail.com>
2024-05-31 16:41:45 +0300

    #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,47 @@ class Rectangle(Base):
         Assign each argument width, height, x and y to the right attribute
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            '''width of rectangle'''
+            return self.__width
+
+        @width.setter
+        def width(self, width):
+            '''setter of rectangle'''
+            self.__width = width
+
+        @property
+        def height(self):
+            '''height of rectangle'''
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            '''setter of rectangle'''
+            self.__height = height
+
+        @property
+        def x(self):
+            '''x of rectangle'''
+            return self.__x
+
+        @x.setter
+        def x(self, x):
+            '''setter of rectangle'''
+            self.__x = x
+
+        @property
+        def y(self):
+            '''y of rectangle'''
+            return self.__y
+
+        @width.setter
+        def y(self, y):
+            '''setter of rectangle'''
+            self.__y = y
