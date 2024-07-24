a2c49906377074a3e0919c640301e8d43fe425ca
Seif Allah <saifeldarageely@gmail.com>
2024-07-24 10:57:49 +0300

    -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT g.`genre_id`, s.`title`
+SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        RIGHT JOIN `tv_show_genres` AS g
        ON s.`id` = g.`show_id`
